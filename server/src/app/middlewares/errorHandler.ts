041df34a8ca3b0d83a8225e5652266a86c5aa1aa
Giancyril <mijaresgiancyril@gmail.com>
2026-04-01 10:22:42 +0800

    feat: add global error handler middleware

---
+++ server/src/app/middlewares/errorHandler.ts
@@ -0,0 +1,11 @@
+import { Request, Response, NextFunction } from "express";
+
+const errorHandler = (err: any, _req: Request, res: Response, _next: NextFunction) => {
+  console.error(err);
+  res.status(err.status ?? 500).json({
+    success: false,
+    message: err.message ?? "Internal server error",
+  });
+};
+
+export default errorHandler;
