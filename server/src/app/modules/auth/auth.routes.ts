e8a5b94153fdbd51ac7a886699591a6ac045b139
Giancyril <mijaresgiancyril@gmail.com>
2026-04-01 10:24:43 +0800

    feat: add auth routes

---
+++ server/src/app/modules/auth/auth.routes.ts
@@ -0,0 +1,14 @@
+import { Router } from "express";
+import * as controller from "./auth.controller";
+import { authenticate, requireAdmin } from "../../middlewares/auth.middleware";
+
+const router = Router();
+
+router.post("/register", authenticate, requireAdmin, controller.register);
+router.post("/login",    controller.login);
+router.get("/users",     authenticate, requireAdmin, controller.getUsers);
+router.put("/users/:id", authenticate, requireAdmin, controller.updateUser);
+router.delete("/users/:id", authenticate, requireAdmin, controller.deleteUser);
+router.put("/change-password", authenticate, controller.changePassword);
+
+export default router;
